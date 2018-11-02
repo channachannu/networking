783a1fadc7f258deec94ff1fede997deef6684d9
Channabasava H <31216354+channachannu@users.noreply.github.com>
2018-11-02 18:53:54 +0530

    Add files via upload

--- firefox.py
+++ firefox.py
@@ -23,9 +23,9 @@ cursor.execute(select)
 count = cursor.fetchall()
 count1 = count[0][0]
 count2 = count[0][1]
-print count1
-print count2
-select_statement = "select id,url from moz_places where id between 3470 and 'count2';"
+#print count1
+#print count2
+select_statement = "select id,url from moz_places where id between 3547 and 'count2';"
 cursor.execute(select_statement)
 results = cursor.fetchall()
 #date(last_visit_date/1000000,'unixepoch')
--- ign.tcl
+++ ign.tcl
@@ -13,24 +13,55 @@ proc finish {} {
 	
         exit 0
 }
+
 set node1 [$ns node]
 $ns at 0.0 "$node1 label localhost"
-set fp [open "idurl.txt" r]
-set content [read $fp]
-puts $content
+
+set ipfile [open "ip.txt" r]
+set ipadd [read $ipfile]
+puts $ipadd
+set count1 1
 set count 2
-array set id {}
+set urlfile [open "imp.txt" r]
+set urladd [read $urlfile]
+puts $urladd
+
+array set ip {}
 array set url {}
-foreach i $content {
-	if {$count%2!=0} {
-		set n($i) [$ns node]
-		puts n($i)
-		$ns at 0.0 "$n($i) label $i"
-		$ns duplex-link $n($i) $node1 10Mb 10ms DropTail
+
+foreach j $ipadd {
+	set end($j) [$ns node]
+	$ns duplex-link $end($j) $node1 10Mb 10ms DropTail
+	$ns at 0.0 "$end($j) label end-point($count1)"
+	incr count1
+	foreach i $urladd {
+		if {$count%2!=0} {
+			set n($i) [$ns node]
+			puts n($i)
+			$ns at 0.0 "$n($i) label $i"
+			$ns duplex-link $n($i) $end($j) 10Mb 10ms DropTail
 	} 
 	incr count
 	
 }
+
+}
+
+for {set n 0} {n < 
+
+#set count 2
+#array set id {}
+#array set url {}
+#foreach i $content {
+#	if {$count%2!=0} {
+#		set n($i) [$ns node]
+#		puts n($i)
+#		$ns at 0.0 "$n($i) label $i"
+#		$ns duplex-link $n($i) $end 10Mb 10ms DropTail
+#	} 
+#	incr count
+	
+#}
 
 
 
